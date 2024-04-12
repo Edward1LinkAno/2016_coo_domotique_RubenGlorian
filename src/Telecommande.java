4de68e13ff3db97d1e486a53777a909c3f672b92
Edward1LinkAno <edward.linkano.gaming@gmail.com>
2024-04-12 11:54:44 +0200

    Update Telecommande.java

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -2,21 +2,35 @@ import java.util.ArrayList;
 
 public class Telecommande {
     private ArrayList<Lampe> lampes ;
+    private ArrayList<Hifi> hifis ;
 
     public Telecommande(){
         this.lampes = new ArrayList<>() ;
+        this.hifis = new ArrayList<>() ;
     }
 
     public void ajouterLampe(Lampe lampe){
         this.lampes.add(lampe) ;
     }
 
+    public void ajouterHifi(Hifi hifi){
+        this.hifis.add(hifi) ;
+    }
+
     public void activerLampe(int indiceLampe){
         this.lampes.get(indiceLampe).allumer() ;
     }
 
+    public void activerHfi(int indiceHifi){
+        this.hifis.get(indiceHifi).allumer() ;
+    }
+
     public void desactiverLampe(int indiceLampe){
         this.lampes.get(indiceLampe).eteindre() ;
+    }
+
+    public void desactiverHifi(int indiceHifi){
+        this.hifis.get(indiceHifi).eteindre() ;
     }
 
     public void activerTout(){
